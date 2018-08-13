c4d3d2c7d657159da1c121cd18b678d62c2c317d
Ryan Ha <ryan@hugsbyha.com>
2018-08-13 10:38:54 -0400

    set the dice roll to show CSS display img'

--- app.js
+++ app.js
@@ -15,17 +15,35 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() *6) +1;
-console.log(dice);
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;  //because of type coercion, we get 'current-0' as the query selector.
 
-document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';//must be a string for html, so it doesn't think it's javascript
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';//must be a string for html, so it doesn't think it's javascript
 
 
 document.querySelector('.dice').style.display = 'none'; //grabs the CSS and sets the property of it.
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';   //could use querySelector, but used getElementById instead. 
+document.getElementById('current-1').textContent = '0';
 
+
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {  //using an anymous function
+
+	//1. Randon number
+	var dice = Math.floor(Math.random() * 6) + 1;  //because setting var dice here, only this function has acces to 'dice variable'
+
+	//2. Display the result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+
+	//3. Update the round score IF the rolled number was not a 1
+
+});
 
 
 
