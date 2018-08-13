311bfd5bf2a3bdec71c37877f03cbda736c7afe5
Ryan Ha <ryan@hugsbyha.com>
2018-08-13 10:13:34 -0400

    querySelector review

--- app.js
+++ app.js
@@ -13,10 +13,38 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
 dice = Math.floor(Math.random() *6) +1;
 console.log(dice);
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;  //because of type coercion, we get 'current-0' as the query selector.
+
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';//must be a string for html, so it doesn't think it's javascript
+
+
+document.querySelector('.dice').style.display = 'none'; //grabs the CSS and sets the property of it.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
